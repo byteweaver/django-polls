c1f1078013378040c7fdee325287484253476572
noxan <noxan@redmoonstudios.de>
2012-09-19 00:23:33 +0200

    add url for poll detail view

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, url
 
-from views import PollListView
+from views import PollDetailView, PollListView
 
 urlpatterns = patterns('',
     url(r'^$', PollListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', PollDetailView.as_view(), name='detail'),
 )
