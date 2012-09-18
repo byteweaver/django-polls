17c5befd8202e16df21a4691f66fe30c8bf6227f
noxan <noxan@redmoonstudios.de>
2012-09-18 23:31:21 +0200

    add unicode function to poll and choice models

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Poll(models.Model):
     question = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return self.question
+
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.choice
