e28c2e80ae4baab97741051911f66c43012d3dde
noxan <noxan@redmoonstudios.de>
2012-09-18 23:34:44 +0200

    add newlines to be pep8 conform

--- polls/models.py
+++ polls/models.py
@@ -9,12 +9,14 @@ class Poll(models.Model):
     def __unicode__(self):
         return self.question
 
+
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=255)
 
     def __unicode__(self):
         return self.choice
+
 
 class Vote(models.Model):
     user = models.ForeignKey(User)
