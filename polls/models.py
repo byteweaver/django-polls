04709ac834a1105886a104c2b3abf1be25eb0903
noxan <noxan@redmoonstudios.de>
2012-09-18 23:16:18 +0200

    add basic model for polls and choices

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=255)
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=255)
