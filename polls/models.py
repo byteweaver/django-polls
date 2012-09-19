341f4402af7b1cb9901ba8cf0537bceb508f9777
noxan <noxan@redmoonstudios.de>
2012-09-19 05:49:47 +0200

    add unicode function to vote model

--- polls/models.py
+++ polls/models.py
@@ -14,14 +14,23 @@ class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=255)
 
+    def count_votes(self):
+        return self.vote_set.count()
+
     def __unicode__(self):
         return self.choice
+
+    class Meta:
+        ordering = ['choice']
 
 
 class Vote(models.Model):
     user = models.ForeignKey(User)
     poll = models.ForeignKey(Poll)
     choice = models.ForeignKey(Choice)
+
+    def __unicode__(self):
+        return u'Vote for %s' % (self.choice)
 
     class Meta:
         unique_together = (('user', 'poll'))
