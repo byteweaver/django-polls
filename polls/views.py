d9f3ac579b093229a7c8f7a1e8ad2519430b459c
noxan <noxan@redmoonstudios.de>
2012-09-19 05:55:26 +0200

    implement post handling for voting in poll vote view

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import DetailView, ListView, RedirectView
 from django.core.urlresolvers import reverse_lazy
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 
-from models import Poll
+from models import Choice, Poll, Vote
 
 
 class PollListView(ListView):
@@ -18,5 +20,13 @@ class PollDetailView(DetailView):
 
 
 class PollVoteView(RedirectView):
+    def post(self, request, *args, **kwargs):
+        poll = Poll.objects.get(id=kwargs['pk'])
+        user = request.user
+        choice = Choice.objects.get(id=request.POST['choice_pk'])
+        Vote.objects.create(poll=poll, user=user, choice=choice)
+        messages.success(request, _("Thanks for your vote."))
+        return super(PollVoteView, self).post(request, *args, **kwargs)
+
     def get_redirect_url(self, **kwargs):
         return reverse_lazy('polls:detail', args=[kwargs['pk']])
