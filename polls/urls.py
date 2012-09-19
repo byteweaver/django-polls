420fa79327686b5aec4f76c416cfcdc99342f2fd
noxan <noxan@redmoonstudios.de>
2012-09-19 05:55:59 +0200

    add login_required decorator to poll vote view

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from views import PollDetailView, PollListView, PollVoteView
 
@@ -6,5 +7,5 @@ from views import PollDetailView, PollListView, PollVoteView
 urlpatterns = patterns('',
     url(r'^$', PollListView.as_view(), name='list'),
     url(r'^(?P<pk>\d+)/$', PollDetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>\d+)/vote/$', PollVoteView.as_view(), name='vote'),
+    url(r'^(?P<pk>\d+)/vote/$', login_required(PollVoteView.as_view()), name='vote'),
 )
