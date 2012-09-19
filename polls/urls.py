ceafa035c409590db35fbbf3aa0f8de4004da580
noxan <noxan@redmoonstudios.de>
2012-09-19 05:55:02 +0200

    add url for poll vote view

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, url
 
-from views import PollDetailView, PollListView
+from views import PollDetailView, PollListView, PollVoteView
+
 
 urlpatterns = patterns('',
     url(r'^$', PollListView.as_view(), name='list'),
     url(r'^(?P<pk>\d+)/$', PollDetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/vote/$', PollVoteView.as_view(), name='vote'),
 )
